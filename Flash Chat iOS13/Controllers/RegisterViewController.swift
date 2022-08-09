6fa4f7b611a504c67c607ad10485106ed8bbd1f2
Markos da Silva <markosrsilva@gmail.com>
2022-08-09 10:34:49 -0300

    feature: completed project

--- Flash Chat iOS13/Controllers/RegisterViewController.swift
+++ Flash Chat iOS13/Controllers/RegisterViewController.swift
@@ -7,13 +7,24 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class RegisterViewController: UIViewController {
-
+    
     @IBOutlet weak var emailTextfield: UITextField!
     @IBOutlet weak var passwordTextfield: UITextField!
     
     @IBAction func registerPressed(_ sender: UIButton) {
+        if let email = emailTextfield.text, let password = passwordTextfield.text {
+            Auth.auth().createUser(withEmail: email, password: password) { result, error in
+                if let e = error {
+                    print(e)
+                    return
+                }
+                
+                self.performSegue(withIdentifier: K.registerSegue, sender: self)
+            }
+        }
     }
     
 }
